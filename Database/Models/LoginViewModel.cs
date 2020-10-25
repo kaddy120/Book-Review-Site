41bd0ff5bcefaa9df1b45595b7abdb9799c0dc5d
kaddy120 <kaddy120@gmail.com>
2020-10-25 23:27:47 +0200

    Trambowyg text editor lib

---
+++ Database/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Database.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Database/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Database.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(60)]
+        public string Name { get; set; }
+
+    }
+}
